2455ac978d515e999ea85caa67815d6299ca2ac5
KaterynaKolomiiets <kateryna.kolomiiets.official@gmail.com>
2021-12-18 14:08:20 +0200

    hw2

--- model/index.js
+++ model/index.js
@@ -1,15 +1,83 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises");
+const contacts = require("./contacts.json");
+const path = require("path");
+const res = require("express/lib/response");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const pathName = path.join(__dirname + "/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    let data = await fs.readFile(pathName);
+    let parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    let data = await fs.readFile(pathName, "utf-8");
+    console.log(data);
+    let parsedData = JSON.parse(data);
+    return (response = parsedData.find(
+      (item) => (item.id).toString() === (contactId).toString()
+    ));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    let data = await fs.readFile(pathName, "utf-8");
+    let parsedData = JSON.parse(data);
+    let filteredData = parsedData.filter(
+      (item) => item.id.toString() !== contactId.toString()
+    );
+    if (filteredData.length !== parsedData.length) {
+      fs.writeFile(pathName, JSON.stringify(filteredData))
+      return { message: "contact deleted" };
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  const data = await fs.readFile(pathName, "utf-8");
+  const parsedData = JSON.parse(data);
+  parsedData.push(newContact);
+  fs.writeFile(pathName, JSON.stringify(parsedData));
+  return newContact;
+};
+
+const updateContact = async (body, contactId) => {
+  let data = await fs.readFile(pathName, "utf-8");
+  let parsedData = JSON.parse(data);
+  let contactToUpdate = parsedData.find(item => (item.id).toString() === (contactId).toString())
+  if (!contactToUpdate) return
+  const newContact = {
+    id: contactToUpdate.id,
+    name: body.name,
+    email: body.email,
+    phone: body.phone
+  }
+  parsedData.splice(parsedData.indexOf(contactToUpdate), 1, newContact);
+  fs.writeFile(pathName, JSON.stringify(parsedData) )
+  return newContact
+
+  
+  
+};
 
 module.exports = {
   listContacts,
@@ -17,4 +85,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
